473b7f5b96aa5d9f0e8f91b44992db6df8a3b46a
weiweiman <wisecck@gmail.com>
2026-09-11 21:35:42 +0800

    Remove temporary API diagnostics

--- api/health.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  return res.status(200).json({ ok: true, runtime: 'vercel-node', ts: Date.now() });
-};
